9112a89a09e4e20e382dabe5cb2f6ebd51c967cf
BeygonK <kipkoechbeygon@gmail.com>
2023-07-19 00:25:47 +0300

    typescript basics

---
+++ dist/basics.js
@@ -0,0 +1,9 @@
+"use strict";
+let id = 5;
+let company = "Denis Beygon";
+let isPublished = true;
+let x = 'Helo';
+x = true;
+// Arrays
+let ids = [1, 2, 3, 4];
+ids.push(6);
---
+++ src/basics.ts
@@ -0,0 +1,11 @@
+let id: number = 5;
+let company: string = "Denis Beygon"
+let isPublished: boolean = true
+let x: any = 'Helo'
+x = true
+
+// Arrays
+let ids: number[] = [1,2,3,4];
+ids.push(6)
+
+//Tuples
